c6b340b818cbcec427114a8931bf390f59dadb94
HeDaGi <151563977+nath162@users.noreply.github.com>
2024-07-03 15:16:16 +0200

    Add files via upload

--- a*.py
+++ a*.py
@@ -1,10 +1,14 @@
-
-
 grid= [ ["S","O","0"],
         [ "O","O","O"],
         [ "O","O","E"],
         ]
+setdistance = set()
+startp=()
+endp =()
+algo_a_commencer = False
+cact = ()
 def dist(grid):
+    global startp,endp
     for i in range(len(grid)):
         for j in range(len(grid)):
             if grid[i][j] =="S":
@@ -12,6 +16,29 @@ def dist(grid):
                 y = i
             elif grid[i][j] == "E":
                 xe = j
-                ye = i  
-    return abs(x-xe)+abs(y-ye)
-print(dist(grid))
+                ye = i
+    startp =(x,y)
+    endp = (xe,ye)  
+    dist = abs(x-xe)+abs(y-ye)
+    setdistance.add(("S",dist))
+    setdistance.add(("E",0))
+    print(endp)
+
+def dist_chaque_cellule(grid):
+    count = 0
+    for i in range(len(grid)):
+        for j in range(len(grid)):
+            if grid[i][j] == "O":
+                count += 1
+                xo = i
+                yo=j 
+                dist = abs(xo-endp[0])+abs(yo-endp[1])
+                setdistance.add(("o"+str(count),dist))
+def celluleactuelle(grid):
+    if not algo_a_commencer:
+        cact = startp
+def voisin(grid):
+    pass
+
+dist(grid)
+print(dist_chaque_cellule(grid))
